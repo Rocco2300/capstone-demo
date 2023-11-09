58b3731e1e33c26a6364e5e2d03f2845e0fef83b
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-11-09 21:44:31 +0200

    basic window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 
-#include <GL/glut.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 
-int main() {
-  std::cout << "Hello world!\n";
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  return 0;
+    glutSwapBuffers();
+}
+
+void reshape(int width, int height) {
+    if (height == 0) {
+        height = 1;
+    }
+    float ratio = width * 1.f / height;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, width, height);
+    gluPerspective(45.f, ratio, 0.1f, 100.f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 200);
+    glutInitWindowSize(400, 400);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutCreateWindow("Capstone Demo");
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "Error initializing glew!\n";
+        std::cerr << glewGetErrorString(err) << '\n';
+        return 1;
+    }
+
+    glutDisplayFunc(render);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(render);
+
+    glutMainLoop();
+
+    return 0;
 }
